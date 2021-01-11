2dcc3f847a4bfb3afbb82f427dc33a5d61da4963
Zack Sheppard <zack@zacksheppard.com>
2021-01-11 16:04:49 +0000

    Initial setup for a koa apollo server

---
+++ src/router.ts
@@ -0,0 +1,4 @@
+import { ApolloServer } from "apollo-server-koa";
+
+const router = new ApolloServer({});
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import * as Koa from "koa";
+import * as helmet from "koa-helmet";
+import "reflect-metadata";
+
+import router from "./router";
+
+const app = new Koa();
+const port = process.env.PORT || 8080;
+
+app.use(helmet());
+
+router.applyMiddleware({ app, cors: {}, bodyParserConfig: {} });
+
+app.listen(port, () => {
+  console.log(`🚀 API listening on the port ${port}`);
+});
